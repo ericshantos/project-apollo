3ab5e30c5347b19971188a1384a09560d54dd300
ericshantos <ericshantos@gmail.com>
2026-06-23 17:29:19 -0300

    feat(dqn): increase DQN hidden layers to 128 neurons

--- agent/agents/dqn_agent.py
+++ agent/agents/dqn_agent.py
@@ -7,6 +7,9 @@ from ..callbacks import ApolloMetricsCallback
 
 class DQNAgent:
     def __init__(self, env, config) -> None:
+
+        policy_kwargs = dict(net_arch=[128, 128])
+
         self._model = DQN(
             policy="MlpPolicy",
             env=env,
@@ -16,6 +19,7 @@ class DQNAgent:
             gamma=config.gamma,
             tensorboard_log="./logs/tensorboard",
             learning_starts=config.learning_starts,
+            policy_kwargs=policy_kwargs,
             target_update_interval=config.target_update_interval,
             verbose=1,
         )
