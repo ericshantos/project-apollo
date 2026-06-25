a07ef0c91f0b9bb73df8319f0551a28fe5ed32a0
ericshantos <ericshantos@gmail.com>
2026-06-25 19:03:47 -0300

    refactor: centralize danger analysis in GameWorld

--- env/reward.py
+++ env/reward.py
@@ -1,21 +1,30 @@
 from configs import cfg
 
-from .game_world import GameWorld
 from .action_space import ActionMap
+from .danger_model import DangerModel
+from .game_world import GameWorld
+from .toroidal_space import ToroidalSpace
 
 
 class RewardFunction:
-    def __init__(self) -> None:
-        self.previous_score: int
-        self.previous_lives: int
+    def __init__(self, space: ToroidalSpace) -> None:
+        self.danger_model = DangerModel(space)
 
-        self.reset()
+        self.previous_score = 0
+        self.previous_lives = cfg.game.max_lives
+        self.previous_risk = 0.0
 
     def reset(self) -> None:
         self.previous_score = 0
         self.previous_lives = cfg.game.max_lives
+        self.previous_risk = 0.0
 
-    def compute(self, world: GameWorld, action: ActionMap) -> float:
+    def compute(
+        self,
+        world: GameWorld,
+        action: ActionMap,
+    ) -> float:
+
         reward = cfg.reward.survive_step
 
         current_score = world.score
@@ -26,6 +35,9 @@ class RewardFunction:
 
         self.previous_score = current_score
 
+        # -------------------------
+        # LIVES
+        # -------------------------
         current_lives = world.player_lives
 
         if current_lives < self.previous_lives:
@@ -33,6 +45,9 @@ class RewardFunction:
 
         self.previous_lives = current_lives
 
+        # -------------------------
+        # ACTION COST
+        # -------------------------
         if action.rotate_left:
             reward -= 0.001
 
@@ -48,7 +63,28 @@ class RewardFunction:
         if action.hyperspace:
             reward -= 0.01
 
+        # -------------------------
+        # DANGER SHAPING
+        # -------------------------
+        analysis = self.danger_model.analyze(
+            world.player,
+            world.asteroids,
+        )
+
+        current_risk = analysis.top_risk
+
+        reward -= 0.05 * current_risk
+
+        reward += 0.02 * (
+            self.previous_risk - current_risk
+        )
+
+        self.previous_risk = current_risk
+
+        # -------------------------
+        # TERMINAL
+        # -------------------------
         if world.is_done():
-            reward -= 10
+            reward -= 10.0
 
         return reward
